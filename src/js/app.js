ac95ac7fa1cd17a5f0ff056564c83ff11789fbe9
Royal-Aver <royal_aver@rambler.ru>
2022-02-28 22:12:19 +0300

    added burger

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,12 @@ import * as flsFunction from "./modules/function.js";
 
 flsFunction.isWebp();
 
+$(function(){
+  $('.burger').on('click', function(){
+    $('.header__list').toggleClass(".header__list--active")
+  })
+})
+
 new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
